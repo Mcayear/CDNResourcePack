beb3b97312527601fb41cc8d036a95c99585c8a8
pixelwhiz <daffa01futn@gmail.com>
2025-06-08 02:46:23 +0700

    updated to protocol version 800

--- src/main/java/cn/vusv/cdnresourcepack/EventListener.java
+++ src/main/java/cn/vusv/cdnresourcepack/EventListener.java
@@ -18,22 +18,24 @@ import java.util.Map;
  * NukkitExamplePlugin Project
  */
 public class EventListener implements Listener {
+
     private final CDNResourcePack plugin;
-    private List<ResourcePacksInfoPacket.CDNEntry> CDNEntries = new ObjectArrayList();
 
     public EventListener(CDNResourcePack plugin) {
         this.plugin = plugin;
     }
 
-    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false) //DON'T FORGET THE ANNOTATION @EventHandler
+    @EventHandler(priority = EventPriority.NORMAL)
     public void onPacket(DataPacketSendEvent event) {
-        if (event.getPacket().packetId() == ProtocolInfo.RESOURCE_PACKS_INFO_PACKET && event.getPacket() instanceof ResourcePacksInfoPacket) {
+        DataPacket packet = event.getPacket();
+        if (packet instanceof ResourcePacksInfoPacket && packet.packetId() == ProtocolInfo.RESOURCE_PACKS_INFO_PACKET) {
             if (!((ResourcePacksInfoPacket) event.getPacket()).getCDNEntries().isEmpty()) return;
             Iterator<Map.Entry<String, Object>> iterator = plugin.urlpackConfig.getAll().entrySet().iterator();
             if (!iterator.hasNext()) return;
             event.setCancelled();
 
-            DataPacket proxyPacket = event.getPacket().clone();
+            ResourcePacksInfoPacket proxyPacket = (ResourcePacksInfoPacket) packet.clone();
+            List<ResourcePacksInfoPacket.CDNEntry> cdnEntries = new ObjectArrayList<>();
 
             while (iterator.hasNext()) {
                 Map.Entry<String, Object> entry = iterator.next();
@@ -41,15 +43,14 @@ public class EventListener implements Listener {
                 // UUIDVersion是资源包的UUID和版本的组合，格式为UUID_Version。仅当客户端尚未缓存资源包时，客户端才会尝试下载它。
                 String UUIDVersion = entry.getKey();
                 // URL是从中下载资源包的URL。此URL必须提供包含manifest.json文件的zip文件，该文件位于另一个文件夹中。manifest不能位于zip文件的根目录。
-                String URL         = String.valueOf(entry.getValue());
-                plugin.getLogger().info("CDN:\nUUIDVersion: "+UUIDVersion+"\nURL: "+URL);
-                CDNEntries.add(new ResourcePacksInfoPacket.CDNEntry(UUIDVersion, URL));
+                String URL = String.valueOf(entry.getValue());
+                plugin.getLogger().info("CDN:\nUUIDVersion: " + UUIDVersion + "\nURL: " + URL);
+                cdnEntries.add(new ResourcePacksInfoPacket.CDNEntry(UUIDVersion, URL));
             }
 
-            ((ResourcePacksInfoPacket) proxyPacket).setCDNEntries(CDNEntries);
+            proxyPacket.setCDNEntries(cdnEntries);
             proxyPacket.encode();
             event.getPlayer().dataPacket(proxyPacket);
         }
-
     }
 }
